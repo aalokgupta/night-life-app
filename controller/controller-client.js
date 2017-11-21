5874878a452161dde00afda9f4d0db1586e964e4
aalokgupta <aalokgupta02@gmail.com>
2017-11-21 20:48:23 +0530

    improvement in UI and functionality

--- controller/controller-client.js
+++ controller/controller-client.js
@@ -1,43 +1,145 @@
 'use strict';
 
-
-
   var btn_search = document.querySelector('.search-btn');
   var text_search = document.querySelector('.input-search-text');
   var searchResult;
-  var myModule = angular.module("searchModule", [])
-                        .controller("searchController", function($scope){
-                         var searchModel = [];
-                         var no_of_search_result = Object.keys(searchResult.results[0]).length;
-                         console.log("no of search result is "+no_of_search_result);
-                        });
+  var searchItems = [];
+
+  var myModule = angular.module("searchModule", ['LocalStorageModule'])
+                        .controller("searchController", function($scope, localStorageService){
+                        //  localStorageService.clearAll();
+                        $scope.searchText = 'Type here for search places..';
+
+                        if(localStorageService.isSupported) {
+                            console.log("localStorageService.isSupported is supported");
+                        }
+
+                        // when page load for the first time it does not find any localstorage
+                        if(localStorageService.get("searchItems") === null) {
+                            $scope.searchItems = searchItems;
+                            console.log("Inside if");
+                        }
+                        else {
+                          $scope.searchItems = localStorageService.get("searchItems");
+                          $scope.searchText = localStorageService.get("searchText");
+                          var index = localStorageService.get("click-on");
+                          $scope.searchItems[index].counter++;
+
+                          localStorageService.clearAll();
+                          console.log("Inside else");
+                        }
 
 
+                        console.log("searchItems = "+$scope.searchItems);
+                        // console.log("search Items = "+searchItems);
+                        if(searchResult !== undefined){
+                            var no_of_search_result = Object.keys(searchResult.results[0]).length;
+                            console.log("no of search result is "+no_of_search_result);
+                        }
+
+                        $scope.clickOnJoining = function(searchItem, index){
+                          console.log("click on joining buton at  "+index);
+                          var requested_url = "http://127.0.0.1:8080/authenticate";
+                          // searchItem.counter++;
+                          localStorageService.set("searchItems", $scope.searchItems);
+                          localStorageService.set("click-on", index);
+                          localStorageService.set("searchText", $scope.searchText);
+
+                          console.log("localstorage content = "+$scope.searchItems);
+
+                          // this.requestAjaxFuntion("GET", requested_url, function(result){
+
+                          // });
+                        }
+
+                        $scope.filterSearchResult = function(searchResults, callback){
+                          console.log("inside filterSearchResult");
+                          $scope.searchItems.length  = 0;
+                          console.log("typedef of objs = "+typeof searchResults);
+
+                          console.log("json.results = "+searchResults.results.length);
+                          console.log("json.results = "+searchResults.results);
+                          console.log("json.results = "+searchResults.results[0].name);
+                          console.log("json.results = "+searchResults.results[1].name);
+                          var res = [];
+                          var no_of_result = (searchResults.results.length > 15) ? 15 : searchResults.results.length;
+
+                          for(var i = 0; i < no_of_result; i++) {
+                            var name = searchResults.results[i].name;
+                            var img = searchResults.results[i].icon;
+                            var address = searchResults.results[i].formatted_address;
+                            console.log("i = "+i);
+                            console.log("name = "+name);
+                            console.log("imageLink = "+img);
+                            console.log("desc = "+address);
+                            var json = {placeName: name,
+                                     counter: 0,
+                                     imageLink: img,
+                                     placeDesc: address};
+                            res.push(json);
+                          }
+                           callback(null, res);
+                        }
+
+                        $scope.requestAjaxFuntion = function(method, url, callback){
+                          console.log("inside requestAjaxFuntion");
+                          var xmlhttp = new XMLHttpRequest();
+                          xmlhttp.onreadystatechange = function() {
+                            if(xmlhttp.readyState === 4 && xmlhttp.status === 200){
+                              console.log("response from server "+xmlhttp.responseText);
+                              var objs =   xmlhttp.responseText;
+                              callback(JSON.parse(xmlhttp.responseText));
+                             }
+                           }
+                           xmlhttp.open(method, url, true);
+                           xmlhttp.send();
+                        }
+
+                        $scope.getSearchResult = function() {
+                          if(text_search.value !== ""){
+                            var requested_url = "http://127.0.0.1:8080/search/" + encodeURI(text_search.value);
+                            this.requestAjaxFuntion("GET", requested_url, function(result){
+                              console.log("this = "+$scope);
+                              $scope.filterSearchResult(result, function(err, res){
+                                  if(err){
+                                    console.log("No Result Found");
+                                  }
+                                  else{
+                                    $scope.searchText = text_search.value;
+                                    $scope.searchItems = res;
+                                    $scope.$apply();
+                                  }
+                              });
+                            });
+                          }
+                        }
+
+                        $scope.onClickSearchText = function () {
+                          $scope.searchText = "";
+                        }
+                      });
 
 
-  function listSearchResult(searchResult){
-
-  }
-
-  function sendAjaxRequest(method, url, callback){
-      var xmlhttp = new XMLHttpRequest();
-      xmlhttp.onreadystatechange = function() {
-        if(xmlhttp.readyState === 4 && xmlhttp.status === 200){
-          console.log("response from server "+xmlhttp.responseText);
-          searchResult = xmlhttp.responseText;
-          callback(xmlhttp.responseText);
-        }
-      }
-
-      xmlhttp.open(method, url, true);
-      // xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-      xmlhttp.send();
-  }
-
-  btn_search.addEventListener('click', function(){
-    if(text_search.value !== ""){
-      var requested_url = "http://127.0.0.1:8080/search/" + encodeURI(text_search.value);
-      console.log("requested url = "+requested_url);
-      sendAjaxRequest("GET", requested_url, listSearchResult);
-    }
-  });
+  //
+  // function sendAjaxRequest(method, url, callback){
+  //     var xmlhttp = new XMLHttpRequest();
+  //     xmlhttp.onreadystatechange = function() {
+  //       if(xmlhttp.readyState === 4 && xmlhttp.status === 200){
+  //         // console.log("response from server "+xmlhttp.responseText);
+  //         searchResult = xmlhttp.responseText;
+  //         callback(xmlhttp.responseText);
+  //       }
+  //     }
+  //
+  //     xmlhttp.open(method, url, true);
+  //     // xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  //     xmlhttp.send();
+  // }
+  //
+  // btn_search.addEventListener('click', function(){
+  //   if(text_search.value !== ""){
+  //     var requested_url = "http://127.0.0.1:8080/search/" + encodeURI(text_search.value);
+  //     // console.log("requested url = "+requested_url);
+  //     // sendAjaxRequest("GET", requested_url, listSearchResult);
+  //   }
+  // });
--- server.js
+++ server.js
@@ -1,19 +1,72 @@
 const express = require('express');
 const request = require('request');
+var passport = require('passport');
+var Strategy = require('passport-twitter').Strategy;
+var expressSession = require('express-session');
 var app = express();
 
-const API_KEY = "AIzaSyDF19dkByvXcwO-iHUSTxge4i1zcsR8Sd4";
+const GOOGLE_API_KEY = "AIzaSyDF19dkByvXcwO-iHUSTxge4i1zcsR8Sd4";
+const TWITTER_API_KEY = "ZMNIsIkRsV0YSVrfrvj4Gdaey";
+const TWITTER_API_SECRET = "iJSWlCocDxf4VtJUuoADOhvRE5zAnKi79KC2xTpLURiQ5FQgPS";
+
+
+passport.use(new Strategy({
+  consumerKey: TWITTER_API_KEY,
+  consumerSecret: TWITTER_API_SECRET,
+  callbackURL: "http://127.0.0.1:8080/auth/twitter/callback"
+},
+function(token, tokenSecret, profile, cb){
+  return cb(null, profile);
+}
+));
+
+passport.serializeUser(function(user, cb){
+    cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb){
+  cb(null, obj);
+});
+
+// app.use(cookieParser());
+// app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use('/public' , express.static(process.cwd() + '/public'));
 app.use('/controller' , express.static(process.cwd() + '/controller'));
+
+//
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  console.log(""+ now + ":" +req.method + " "+req.url);
+  next();
+});
+
+
+app.get('/auth/twitter/callback',
+   passport.authenticate('twitter', {failureRedirect: '/'}),
+        function(req, res) {
+          console.log("Inside twitter call back");
+          res.redirect('/');
+        });
+
 
 app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/public/home.html');
 });
 
+app.get('/auth/twitter',
+   passport.authenticate('twitter'), function(req, res){
+   });
+
+
 
 app.get('/search/:text', (req, res) => {
     console.log("search query = "+req.params.text);
-    var google_url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+ encodeURIComponent(req.params.text) + "&key=" + API_KEY;
+    var google_url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+ encodeURIComponent(req.params.text) + "&key=" + GOOGLE_API_KEY;
     console.log("google url " +google_url);
     request({url: google_url,
             json: true,
@@ -23,19 +76,19 @@ app.get('/search/:text', (req, res) => {
                 console.log("No result found for the search");
               }
               else if(body.status === "OK"){
-                  console.log("response body is "+JSON.stringify(body, undefined, 2));
+                  // console.log("response body is "+JSON.stringify(body, undefined, 2));
                   res.send(JSON.stringify(body, undefined, 2));
               }
           });
-
-});
-
-const port_no = 8080;
-app.listen(8080, () => {
-  console.log("server started listening at port no "+port_no);
 });
 
 
+
+
+  const port_no = 8080;
+  app.listen(8080, () => {
+    console.log("server started listening at port no "+port_no);
+  });
 //
 
 
